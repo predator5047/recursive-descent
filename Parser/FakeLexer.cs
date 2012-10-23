49d5aed9b156a5147108758f7cfa181effa37914
Lavesson <eric.lavesson@gmail.com>
2012-10-24 01:56:12 +0200

    Fake Lexer added, whose Tokenize-method simply returns a preset stream of tokens

---
+++ Parser/FakeLexer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Parser
+{
+    public class FakeLexer : ILexer
+    {
+        public List<Token> LexerOutput { get; set; }
+
+        public PQueue<Token> Tokenize(string input)
+        {
+            var queue = new PQueue<Token>();
+
+            foreach (var token in LexerOutput)
+            {
+                queue.Enqueue(token);
+            }
+
+            return queue;
+        }
+    }
+}
