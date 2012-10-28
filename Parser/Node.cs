938680a91cfd075359e406928ec28a67f6f689c4
Lavesson <eric.lavesson@gmail.com>
2012-10-28 02:43:34 +0100

    Added a AddChild-method that returns the created child as well

--- Parser/Node.cs
+++ Parser/Node.cs
@@ -13,6 +13,13 @@ namespace Parser
             Value = value;
         }
 
+        public Node<T> AddChild(T value)
+        {
+            var node = new Node<T>(value);
+            m_children.Add(node);
+            return node;
+        }
+
         private List<Node<T>> m_children = new List<Node<T>>();
  
         public T Value { get; set; }
