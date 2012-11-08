1cb3e8a0414d295b0e0649b7c4181a6247e0b7f0
Lavesson <eric.lavesson@gmail.com>
2012-11-08 18:01:15 +0100

    Added tests for the lexer

--- ParserTests/LexerTest.cs
+++ ParserTests/LexerTest.cs
@@ -26,9 +26,56 @@ namespace ParserTests
             expected.Enqueue(new Token { TokenType = TokenType.IF, Value = "if" });
 
             var actual = sut.Tokenize(input);
-
             Assert.AreEqual(expected.First().TokenType, actual.First().TokenType);
             Assert.AreEqual(expected.First().Value, actual.First().Value);
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+
+        [Test]
+        public void CanCreateTwoLexemes()
+        {
+            var input = "if(";
+            var expected = new PQueue<Token>();
+            expected.Enqueue(new Token { TokenType = TokenType.IF, Value = "if" });
+            expected.Enqueue(new Token { TokenType = TokenType.LPAR, Value = "(" });
+
+            var actual = sut.Tokenize(input);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected.First().TokenType, actual.First().TokenType);
+            Assert.AreEqual(expected.First().Value, actual.First().Value);
+            Assert.AreEqual(expected.Last().TokenType, actual.Last().TokenType);
+            Assert.AreEqual(expected.Last().Value, actual.Last().Value);
+        }
+
+        [Test]
+        public void CanIdentifyRegexLexem()
+        {
+            var input = "banan";
+            var expected = new PQueue<Token>();
+            expected.Enqueue(new Token { TokenType = TokenType.NAME, Value = "banan" });
+
+            var actual = sut.Tokenize(input);
+            Assert.AreEqual(expected.First().TokenType, actual.First().TokenType);
+            Assert.AreEqual(expected.First().Value, actual.First().Value);
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+
+        [Test]
+        public void IgnoresWhiteSpaceInExpressions()
+        {
+            var input = "if (";
+            var expected = new PQueue<Token>();
+            expected.Enqueue(new Token { TokenType = TokenType.IF, Value = "if" });
+            expected.Enqueue(new Token { TokenType = TokenType.LPAR, Value = "(" });
+
+            var actual = sut.Tokenize(input);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected.First().TokenType, actual.First().TokenType);
+            Assert.AreEqual(expected.First().Value, actual.First().Value);
+            Assert.AreEqual(expected.Last().TokenType, actual.Last().TokenType);
+            Assert.AreEqual(expected.Last().Value, actual.Last().Value);
         }
     }
 }
