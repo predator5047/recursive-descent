096de33f8273b4d1b1a07a9a94dd2acd57c5d33c
Lavesson <eric.lavesson@gmail.com>
2012-11-09 16:18:56 +0100

    Initial commit of compiler project

---
+++ SimpleCompiler/Compiler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Parser;
+
+namespace SimpleCompiler
+{
+    public class Compiler
+    {
+        private Stack<string> m_leaves = new Stack<string>();
+
+        public void Compile(Node<string> input)
+        {
+            TransformToStack(input);
+        }
+
+        private void TransformToStack(Node<string> input)
+        {
+            FetchLeaves(input);
+            m_leaves = new Stack<string>(m_leaves);
+        }
+
+        private void FetchLeaves(Node<string> input)
+        {
+            if (input.Children.Count != 0)
+            {
+                foreach (var child in input.Children)
+                {
+                    TransformToStack(child);
+                }
+            }
+            else
+            {
+                m_leaves.Push(input.Value);
+            }
+        }
+    }
+}
